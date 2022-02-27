c7a3f6cf9bc5e6d5fc52112f58e0b238764b5277
Toxyk <toxyflog.games@gmail.com>
2022-02-27 14:02:51 +0200

    heroku

--- db/index.js
+++ db/index.js
@@ -2,11 +2,14 @@ const {Pool} = require("pg");
 
 const client = new Pool({
 	max: 20,
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	database: process.env.DB_NAME,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
+	connectionString: process.env.DATABASE_URL ||
+		`postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 });
+
+console.log(process.env);
+console.log(process.env.DATABASE_URL);
 
 module.exports = client;
