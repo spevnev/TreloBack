04a085124bc2430175200acce122dec033502d18
Toxyk <toxyflog.games@gmail.com>
2022-03-18 19:52:07 +0200

    redis

--- src/redis/index.js
+++ src/redis/index.js
@@ -1,6 +1,8 @@
 const {createClient} = require("redis");
 
 if (process.env.NODE_ENV !== "test") {
+	console.log(process.env);
+	console.log(process.env.REDISTOGO_URL);
 	const client = createClient({
 		url: process.env.REDISTOGO_URL || "redis://:password@localhost:6379",
 	});
