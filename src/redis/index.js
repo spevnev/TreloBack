dd7a6064262ba2a3f38fd3def677de232a8b74d6
Toxyk <toxyflog.games@gmail.com>
2022-03-18 20:05:30 +0200

    mb

--- src/redis/index.js
+++ src/redis/index.js
@@ -2,12 +2,12 @@ const {createClient} = require("redis");
 
 if (process.env.NODE_ENV !== "test") {
 	const client = createClient({
-		url: process.env.REDISTOGO_URL.match("(redis:\/\/).{1,20}(:.*@.*:[0-9]*\/)").slice(1).join("") || "redis://:password@localhost:6379",
+		url: process.env.REDISTOGO_URL.match("(redis:\/\/).{1,20}:(.*@.*:[0-9]*\/)").slice(1).join("") || "redis://:password@localhost:6379",
 	});
 	console.log("Redis is connected!");
 
 	client.on("error", e => {
-		// throw new Error(e);
+		throw new Error(e);
 	});
 
 	module.exports = client;
