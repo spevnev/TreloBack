8e33a1956c862fb4175e75046658189b355834c8
Toxyk <toxyflog.games@gmail.com>
2022-03-18 19:12:55 +0200

    fucking redis

--- src/redis/index.js
+++ src/redis/index.js
@@ -1,13 +1,14 @@
 const {createClient} = require("redis");
 
 const client = createClient({
-	url: process.env.REDISTOGO_URL || "redis://:password@localhost:6379",
+	url: process.env.REDISTOGO_URL || "redis://redistogo:0d40d763ba134ade31224ec0a0587ebc@sole.redistogo.com:10450",
 });
+console.log("Redis is connected!");
 
 client.on("error", e => {
 	throw new Error(e);
 });
 
-if (process.env.NODE_ENV !== "test") client.connect().then(() => console.log("Redis is connected!"));
+// if (process.env.NODE_ENV !== "test") client.connect().then(() => console.log("Redis is connected!"));
 
 module.exports = client;
